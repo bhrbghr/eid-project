4ba8d54bad34d084f6a3242ea2c8e5b62de6cdfc
bhrbghr <baharmehr871@gmail.com>
2025-04-02 19:43:22 +0330

    basic-design

---
+++ src/db/Database.java
@@ -0,0 +1,46 @@
+package db;
+
+import db.exception.EntityNotFoundException;
+import java.util.ArrayList;
+
+public class Database {
+
+    private static final ArrayList<Entity> entities = new ArrayList<>();
+    private static int nextId = 1;
+
+    private Database() {}
+
+    public static void add(Entity e) {
+        e.id = nextId++;
+        entities.add(e);
+    }
+
+    public static Entity get(int id) {
+        for (Entity e : entities) {
+            if (e.id == id) {
+                return e;
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void delete(int id) {
+        for (Entity e : entities) {
+            if (e.id == id) {
+                entities.remove(e);
+                return;
+            }
+        }
+        throw new EntityNotFoundException("Entity not found to delete.");
+    }
+
+    public static void update(Entity e) {
+        for (int i = 0; i < entities.size(); i++) {
+            if (entities.get(i).id == e.id) {
+                entities.set(i, e);
+                return;
+            }
+        }
+        throw new EntityNotFoundException(e.id);
+    }
+}
